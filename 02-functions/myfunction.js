c0c4a1e7238f4f4997a796b3ffec1654cb45aec1
Frehitzz <fritzharlydegamo@gmail.com>
2026-01-19 16:28:05 +0800

    tsx in functions and arrow functions

---
+++ 02-functions/myfunction.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// using type sript on function
+function numAdd(num) {
+    return num + 5;
+}
+function getUpper(val) {
+    return val.toUpperCase();
+}
+function signUp(name, email, isPaid) { }
+// using typesript on arrow funtion
+var loginUser = function (name, email, isPaid) {
+    if (isPaid === void 0) { isPaid = true; }
+};
+// isPiad: booleant = true  - setting up a default value
+numAdd(5);
+getUpper("hello");
+signUp("Fritz", "@gmail.com", true);
+loginUser("h", "fhd@gmail.com");
---
+++ 02-functions/myfunction.tsx
@@ -0,0 +1,21 @@
+// using type sript on function
+function numAdd(num: number) {
+  return num + 5;
+}
+
+function getUpper(val: string) {
+  return val.toUpperCase();
+}
+
+function signUp(name: string, email: string, isPaid: boolean) {}
+
+// using typesript on arrow funtion
+let loginUser = (name: string, email: string, isPaid: boolean = true) => {};
+// isPiad: booleant = true  - setting up a default value
+
+numAdd(5);
+getUpper("hello");
+signUp("Fritz", "@gmail.com", true);
+loginUser("h", "fhd@gmail.com");
+
+export {};
