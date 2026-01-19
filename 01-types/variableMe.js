cc0f160447f65a17f29858f671d675880c56f79d
Frehitzz <fritzharlydegamo@gmail.com>
2026-01-19 14:27:11 +0800

    types of writing typescript

---
+++ 01-types/variableMe.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// string
+var greetings = "Hello Fritzs";
+console.log(greetings);
+// number
+var mynum = 12;
+console.log(mynum);
+// boolean
+var isLogedIn = false;
+console.log(isLogedIn);
---
+++ 01-types/variableMe.ts
@@ -0,0 +1,14 @@
+// string
+let greetings: string = "Hello Fritzs";
+console.log(greetings);
+
+// number
+let mynum: number = 12;
+
+console.log(mynum);
+
+// boolean
+let isLogedIn: boolean = false;
+console.log(isLogedIn);
+
+export {};
